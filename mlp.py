4eea088e834f778e2763ee9307fd5e481d8ce87a
Louis-Guillaume Gagnon <louis.guillaume.gagnon@gmail.com>
2017-02-09 11:43:52 -0500

    Implement multi-layer perceptron

--- mlp.py
+++ mlp.py
@@ -2,9 +2,18 @@
 
 # pylint: disable=invalid-name, no-member, too-few-public-methods
 
+import logging
+
 import numpy as np
 import theano
 import theano.tensor as T
+
+theano.config.floatX = 'float32'
+# uncomment for easier debugging:
+# theano.config.optimizer='fast_compile'
+
+
+log = logging.getLogger(__name__)
 
 
 class LinearTransformation(object):
@@ -20,10 +29,12 @@ class LinearTransformation(object):
         self.shape = shape
         n_in, n_out = self.shape
 
+        log.debug('Creating LinearTransformation layer with shape n_in=%d, n_out=%d', n_in, n_out)
+
         self.W = theano.shared(
             value=np.random.uniform(
                 low=-np.sqrt(1.0 / n_in),
-                high=-np.sqrt(1.0 / n_in),
+                high=np.sqrt(1.0 / n_in),
                 size=(n_in, n_out),
             ).astype('float32'),
             name='W',
@@ -34,10 +45,79 @@ class LinearTransformation(object):
             name='b',
         )
 
-        # self.output = T.dot(X, self.W) + self.b
-
         self.parameters = [self.W, self.b]
 
     def __call__(self, X):
         """ Create a symbolic expression X * W + b """
         return T.dot(X, self.W) + self.b
+
+
+class MLP(object):
+    """ Multilayer perceptron """
+
+    def __init__(self, structure):
+
+        self.layers = []
+        self.parameters = []
+
+        for ni, no in zip(structure[:-1], structure[1:]):
+            self.layers.append(LinearTransformation((ni, no)))
+            self.parameters += self.layers[-1].parameters
+
+    def __call__(self, X):
+        tensor = X
+        for i, layer in enumerate(self.layers):
+            tensor = layer(tensor)
+            if i < len(self.layers) - 1:
+                tensor = T.nnet.relu(tensor)
+        return tensor
+
+    def training_function(self, lr=0.01):
+        """Compile a theano function for training
+
+        The resulting function takes 2 minibatches, X and Y, as input
+        and outputs the loss. The weights are updated for every
+        minibatch.
+
+        Arguments:
+          lr : (optional) learning rate for weight updated.
+        Returns: a theano function.
+        """
+
+        X = T.matrix('X')
+        Y = T.matrix('Y')
+
+        loss = T.mean(T.pow(self(X) - Y, 2))
+
+        gparams = [T.grad(loss, p) for p in self.parameters]
+
+        updates = [
+            (param, param - lr * gparam)
+            for param, gparam in zip(self.parameters, gparams)
+        ]
+
+        train_fun = theano.function(
+            inputs=[X, Y],
+            outputs=loss,
+            updates=updates,
+        )
+
+        return train_fun
+
+    def test_function(self):
+        """Compile a theano function for testing
+
+        The resulting function takes 1 minibatche X  as input
+        and outputs the prediction.
+
+        Returns: a theano function.
+        """
+
+        X = T.matrix('X')
+
+        test_fun = theano.function(
+            inputs=[X],
+            outputs=self(X)
+        )
+
+        return test_fun
---
+++ test_mlp.py
@@ -0,0 +1,95 @@
+""" Simple regression test for the Densely connected MLP """
+
+#xbimport logging
+#logging.basicConfig(level='DEBUG')
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+import mlp
+
+## Generate the test data
+print '-> Generating the data'
+
+# random generator seed
+np.random.seed(750)
+
+# number of training points
+N_TRAIN = 100000
+
+# number of test points
+N_TEST = 1000
+
+# variance of additive noise
+NOISE_STDDEV = 0.1
+
+# path of training sample
+PATH_TRAIN = 'sine.training.txt'
+
+# path of test sample
+PATH_TEST = 'sine.test.txt'
+
+def __gen_data(n, path):
+    data = np.zeros((n,2), dtype='float32')
+    data[:,0] = np.random.uniform(0, 2*np.pi, n)
+    data[:,1] = np.sin(data[:,0]) + np.random.normal(scale=NOISE_STDDEV, size=n)
+    return data[:,0], data[:,1]
+
+datax, datay = __gen_data(N_TRAIN, PATH_TRAIN)
+testx, testy  = __gen_data(N_TEST, PATH_TEST)
+
+mean = np.mean(datax)
+std = np.std(datax)
+datax -= mean
+datax /= std
+testx -= mean
+testx /= std
+
+datax = np.atleast_2d(datax).T
+datay = np.atleast_2d(datay).T
+testx = np.atleast_2d(testx).T
+
+#create and train the network
+print '-> Building the model'
+
+network = mlp.MLP([1, 300, 1])
+training = network.training_function()
+
+print '-> Training the model'
+
+batch_size=128
+n_train_batches=datax.size/batch_size
+ 
+nepochs = 50
+epoch = 0
+while (epoch < nepochs):
+    
+    losses = np.zeros(n_train_batches)
+
+    for minibatch_index in range(n_train_batches):
+        i0 = minibatch_index * batch_size
+        i1 = (minibatch_index + 1) * batch_size
+        losses[minibatch_index] = training(datax[i0:i1], datay[i0:i1])
+
+    print 'epoch {}: avg. loss = {}'.format(epoch, np.mean(losses))
+    epoch += 1
+
+print '-> Testing the model'
+    
+test = network.test_function()
+outputs = test(testx)
+
+# graph of result
+
+isort = np.argsort(testx[:,0])
+x = testx[isort] * std + mean
+y = testy[isort]
+
+# print np.min(x), np.max(x)
+
+# print test(testx[isort])
+
+plt.plot(x, test(testx[isort]), 'k')
+plt.plot(x, np.sin(x), 'k--')
+plt.scatter(x, y, facecolors='none', edgecolor='r')
+plt.savefig('sine_test.png')
