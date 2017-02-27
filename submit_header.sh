27a3f746303e2e1827e485f818a65359ebccff9b
Louis-Guillaume Gagnon <louis.guillaume.gagnon@gmail.com>
2017-02-27 13:02:49 -0500

    fix submit script

--- submit_header.sh
+++ submit_header.sh
@@ -2,11 +2,6 @@
 
 set -o errexit
 
-module load CUDA
-module load openblas
-module load python/3.5.1
-module load theano
-
 _workdir=$LSCRATCH
 cd $_workdir && pwd
 git clone $HOME/IFT6266-project .
