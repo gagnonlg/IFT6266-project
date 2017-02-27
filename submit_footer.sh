416a60978e8d3a33a57cc435b525c297c6fe3b50
Louis-Guillaume Gagnon <louis.guillaume.gagnon@gmail.com>
2017-02-27 13:38:41 -0500

    fix footer

--- submit_footer.sh
+++ submit_footer.sh
@@ -1,6 +1,8 @@
 # BEGIN FOOTER
 
-cd $HOME
-tar cpzf $(basename $_workdir).tar.gz $_workdir
+cd $_workdir/..
+_results=$(basename $_workdir)
+tar cpzf $_results.tar.gz $_results
+cp $_results.tar.gz $HOME
 
 # END FOOTER
