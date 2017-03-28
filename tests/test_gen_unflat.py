3f70c00fe48acc6db1f8abbcccaacf5a1a537096
Louis-Guillaume Gagnon <louis.guillaume.gagnon@gmail.com>
2017-03-28 11:34:43 -0400

    fix test

--- tests/test_gen_unflat.py
+++ tests/test_gen_unflat.py
@@ -14,7 +14,7 @@ gen = dataset.generate_unflattened(datapath)
 masked, patch = gen.next()
 
 PIL.Image.fromarray(np.transpose(masked, (1, 2, 0)).astype('uint8')).show()
-PIL.Image.fromarray(np.transpose(patch, (1, 2, 0)).astype('uint8')).show()
+PIL.Image.fromarray(patch.reshape(32, 32, 3).astype('uint8')).show()
 
 img_ = dataset.reconstruct_from_unflat(masked, patch)
 PIL.Image.fromarray(img_.astype('uint8')).show()
